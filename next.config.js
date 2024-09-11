babff74767b74c4018d5fff9fcb82aa67ac77b5f
M-Brond <maartenbrondijk@icloud.com>
2024-09-11 13:36:22 +0200

    nextjs flow fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  output: 'export',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/WyntonTrading/',
+  basePath: '/WyntonTrading',
 }
+
+module.exports = nextConfig
